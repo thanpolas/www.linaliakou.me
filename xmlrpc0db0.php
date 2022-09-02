6c5943b22dabb9d87740ba7bd4bac80bb5f16a8f
Thanos Polychronakis <thanpolas@gmail.com>
2022-09-02 14:04:08 +0300

    remove direct hostname references

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,12 +2,12 @@
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>https://linaliakou.me</homePageLink>
+    <homePageLink></homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="https://linaliakou.me/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://linaliakou.me/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://linaliakou.me/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="https://linaliakou.me/xmlrpc.php" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="/xmlrpc.php" />
           </apis>
   </service>
 </rsd>
